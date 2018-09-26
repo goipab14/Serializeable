ca3b18f524b5019d9470ca83cd80ecf6895c2e9d
goipab14 <goipab14@htlkaindorf.at>
2018-09-26 12:43:29 +0200

    created classes schueler and schuelerbl

---
+++ Serialize Intro/src/Schueler.java
@@ -0,0 +1,48 @@
+
+import java.time.LocalDate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pascal
+ */
+public class Schueler {
+    private String name;
+    private int age;
+    private LocalDate birthday;
+
+    public Schueler(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+        this.age = LocalDate.now().getYear() - birthday.getYear();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+}
---
+++ Serialize Intro/src/SchuelerBL.java
@@ -0,0 +1,32 @@
+
+import java.io.File;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pascal
+ */
+public class SchuelerBL {
+   private ArrayList<Schueler> klasse = new ArrayList<>(); 
+   
+   public void add(Schueler s)
+   {
+       klasse.add(s);
+   }
+   
+   public void save(File f)
+   {
+       
+   }
+   
+   public void load(File f)
+   {
+       
+   }
+}
